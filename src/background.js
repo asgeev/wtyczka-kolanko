2fb78be3851759a83ccaf3870881305dfb24c02f
polishghost27 <adamszymanski27@hotmail.com>
2022-06-20 10:39:01 +0200

    More refactoring code

--- src/background.js
+++ src/background.js
@@ -1,26 +1,105 @@
-import { aa } from "./szoi/szoi.js"
-
 const ezdLink = "ezd.nfz-lublin.pl"
 const szoiLink = "www-2.nfz-lublin.pl"
 
-chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 
-    console.log(tabs.url)
+chrome.tabs.onActivated.addListener((activeTabInfo) => {
 
-    const activeTab = tabs[0];
+    chrome.tabs.get(activeTabInfo.tabId, (tab) => {
 
-    console.log(activeTab)
-    const activeTabId = activeTab.id;
+        const currentUrl = tab.url
 
-    console.log(activeTabId)
-
- });
-
+        currentUrlFunction(currentUrl)
+                 
+    })
+})
 
 
 
 
+const currentUrlFunction = (url) => {
 
+    if(!url){
+
+        console.log('link n')
+
+        return
+
+    }else{
+
+        console.log(url)
+
+    }
+
+}
+//     if(!currentUrl()){
+
+//         console.log("Błędny adres strony")
+        
+//         return
+
+//     }else if(currentUrl.contains(ezdLink)){
+
+//         console.log("Aktualna strona to EZD")
+
+//     }else if(currentUrl.contains(szoiLink)){
+
+//         console.log("Aktualna strona to SZOI")
+
+//     }
+
+
+// })
+
+
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+
+//     // console.log(tabs.url)
+
+//     const activeTab = tabs[0];
+
+//     console.log(activeTab.url)
+
+//     // return currentLink = activeTab.url 
+
+
+//     // const activeTabId = activeTab.id;
+
+//     // console.log(activeTabId)
+
+//  });
+
+
+
+
+
+// chrome.tabs.onActivated.addListener((activeInfo) => {
+//     chrome.tabs.get(activeInfo.tabId, (tab) => {
+        
+//         currentLink = tab.url;
+//         console.log("you are here: "+ currentLink);
+
+//     });
+// });
+
+//  chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+    
+//     if (tab.active && change.url) {
+//         console.log("you are here: "+change.url);           
+//     }
+// });
+
+// console.log(currentLink)
+
+
+// if(currentLink){
+
+//     console.log("Empty url")
+
+// }else{
+
+//     console.log(currentLink)
+
+//  }
 
 // const currentPage = () => {
 
