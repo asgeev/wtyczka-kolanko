b0d2f17982831e46654e2b685405c7c14e014806
polishghost27 <adamszymanski27@hotmail.com>
2022-06-20 15:41:54 +0200

    Better refactoring code

--- src/background.js
+++ src/background.js
@@ -1,146 +1,76 @@
 const ezdLink = "ezd.nfz-lublin.pl"
 const szoiLink = "www-2.nfz-lublin.pl"
 
+ 
 
 chrome.tabs.onActivated.addListener((activeTabInfo) => {
 
     chrome.tabs.get(activeTabInfo.tabId, (tab) => {
 
+        // console.log(activeTabInfo.tabId)
+
         const currentUrl = tab.url
 
-        currentUrlFunction(currentUrl)
+        // console.log(activeTabInfo)
+
+        checkCurrentUrl(currentUrl, activeTabInfo.tabId)
                  
     })
 })
 
+chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
+
+        if (change.status == 'complete'){
+
+            // console.log("you are here: "+ tab.url); 
+            
+            checkCurrentUrl(tab.url, tabId)
+        }
+    });
 
 
 
-const currentUrlFunction = (url) => {
+let checkCurrentUrl = (url, currentTabId) => {
 
-    if(!url){
+        // console.log('Url', url)
+        // console.log('TabId', currentTabId )
 
-        console.log('link n')
+        if(!url){
 
-        return
+            console.log("Błędny adres strony")
+     
 
-    }else{
+        }else if(url.includes(ezdLink)){
 
-        console.log(url)
+            chrome.scripting.executeScript({
+                target: {tabId: currentTabId, allFrames: true},
+                files: ['./src/ezd/ezd.js']
+            })
 
-    }
+            chrome.scripting.insertCSS({
+                target: {tabId: currentTabId, allFrames: true},
+                files: ['./src/ezd/styleEZD.css']
+            })
 
+            // console.log("Link do EZD")
+
+        }else if(url.includes(szoiLink)){
+
+            chrome.scripting.executeScript({
+                target: {tabId: currentTabId, allFrames: true},
+                files: ['./src/szoi/szoi.js']
+            })
+
+            chrome.scripting.insertCSS({
+                target: {tabId: currentTabId, allFrames: true},
+                files: ['./src/szoi/styleSZOI.css']
+            })
+
+            // console.log("Link do SZOI")
+   
+        }else {
+
+            console.log("Błędny adres strony")
+
+        }
 }
-//     if(!currentUrl()){
-
-//         console.log("Błędny adres strony")
-        
-//         return
-
-//     }else if(currentUrl.contains(ezdLink)){
-
-//         console.log("Aktualna strona to EZD")
-
-//     }else if(currentUrl.contains(szoiLink)){
-
-//         console.log("Aktualna strona to SZOI")
-
-//     }
-
-
-// })
-
-
-// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-
-//     // console.log(tabs.url)
-
-//     const activeTab = tabs[0];
-
-//     console.log(activeTab.url)
-
-//     // return currentLink = activeTab.url 
-
-
-//     // const activeTabId = activeTab.id;
-
-//     // console.log(activeTabId)
-
-//  });
-
-
-
-
-
-// chrome.tabs.onActivated.addListener((activeInfo) => {
-//     chrome.tabs.get(activeInfo.tabId, (tab) => {
-        
-//         currentLink = tab.url;
-//         console.log("you are here: "+ currentLink);
-
-//     });
-// });
-
-//  chrome.tabs.onUpdated.addListener((tabId, change, tab) => {
-    
-//     if (tab.active && change.url) {
-//         console.log("you are here: "+change.url);           
-//     }
-// });
-
-// console.log(currentLink)
-
-
-// if(currentLink){
-
-//     console.log("Empty url")
-
-// }else{
-
-//     console.log(currentLink)
-
-//  }
-
-// const currentPage = () => {
-
-//     console.log(aa)
-
-//     if(currentLink){
-
-//         console.log(currentLink.host.toString())
-    
-//         currentLink = currentLink.host.toString()
-    
-//         if(currentLink.includes(ezdLink)){
-    
-//             console.log("Link do ezd")
-
-//             return
-    
-    
-//         }else if(currentLink.includes(szoiLink)){
-    
-//             console.log("Link do szoi")
-
-//             return
-    
-//         }else{
-    
-//             console.log("Błędny adres strony")
-
-//             return
-    
-//         }
-    
-//     }else{
-    
-//         console.log("Brak adresu strony")
-
-//         return
-    
-//     }     
-
-// }
-
-// window.onload = currentPage()
-
--- src/ezd/ezd.js
+++ src/ezd/ezd.js
@@ -1,9 +1,19 @@
 
+document.addEventListener("load", () => {
 
-const container = document.getElementsByClassName("pnlDodajNotatke")
-const notebtndiv = Object.assign(document.createElement('div'), { className: 'notebtndiv' })
+    const container = document.querySelector(".pnlDodajNotatke")
+// const notebtndiv = Object.assign(document.createElement('div'), { className: 'notebtndiv' })
 
+    if(container) {
 
+        console.log(container)
 
-container[0].append(notebtndiv)
-notebtndiv.innerHTML = "<button class = 'notebtn'>Opcja</button><button class = 'notebtn'>Opcja</button><button class = 'notebtn'>Opcja</button><button class = 'notebtn'>Opcja</button>";
+        const notebtndiv = document.createElement('div')
+        
+        container[0].append(notebtndiv)
+        
+        notebtndiv.innerHTML = "<button class = 'notebtn'>Opcja</button><button class = 'notebtn'>Opcja</button><button class = 'notebtn'>Opcja</button><button class = 'notebtn'>Opcja</button>";
+
+}
+
+})
